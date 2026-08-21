769ecd1573fdab2fd99127d1a993e87d9231cb28
zakiabbas133 <zaki.abbas@intercargo.com>
2026-08-21 11:35:08 +0100

    Unused imports removed

--- src/pages/MenuItemDetails/MenuItemDetails.tsx
+++ src/pages/MenuItemDetails/MenuItemDetails.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useLayoutEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import { Link, Navigate, useParams } from "react-router-dom";
 
